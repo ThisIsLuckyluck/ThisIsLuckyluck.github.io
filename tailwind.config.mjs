ec8cf4208bdaba849a34466ae21fc3e5eec57e44
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-09-24 12:00:52 +0200

    footer finished adding images for social media in the way for adding dark/light mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors : {
+				darktheme: "#0f172a",
+				lighttheme: "#f5f5f5",
+			}
+		},
 	},
 	plugins: [],
 }
